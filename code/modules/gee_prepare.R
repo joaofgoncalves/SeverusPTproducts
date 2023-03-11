0637fb45d4ea0594a1f324a71e3bbbd175ec26af
João Gonçalves <joaofgo@gmail.com>
2023-03-11 22:16:28 +0000

    Added cloud masks and scale functions for MODIS

--- code/modules/gee_prepare.R
+++ code/modules/gee_prepare.R
@@ -44,7 +44,7 @@ maskClouds_LT5 <- function(img){
   mask_clo <- qa$bitwiseAnd(cloudBitMask)$eq(0)
   mask_sha <- qa$bitwiseAnd(cloudShadowBitMask)$eq(0)
   
-  return(img$updateMask( mask_clo$And(mask_sha) ))
+  return(img$updateMask(mask_clo$And(mask_sha)))
 }
 
 maskClouds_LT7 <- function(img){
@@ -60,7 +60,7 @@ maskClouds_LT7 <- function(img){
   mask_clo <- qa$bitwiseAnd(cloudBitMask)$eq(0)
   mask_sha <- qa$bitwiseAnd(cloudShadowBitMask)$eq(0)
   
-  return(img$updateMask( mask_clo$And(mask_sha) ))
+  return(img$updateMask(mask_clo$And(mask_sha)))
 }
 
 # Works with L1 and L2!!
@@ -77,7 +77,7 @@ maskClouds_LT8 <- function(img){
   mask_clo <- qa$bitwiseAnd(cloudBitMask)$eq(0)
   mask_sha <- qa$bitwiseAnd(cloudShadowBitMask)$eq(0)
   
-  return(img$updateMask( mask_cir$And(mask_clo)$And(mask_sha) ))
+  return(img$updateMask(mask_cir$And(mask_clo)$And(mask_sha)))
 }
 
 maskClouds_LT9 <- function(img){
@@ -93,25 +93,70 @@ maskClouds_LT9 <- function(img){
   mask_clo <- qa$bitwiseAnd(cloudBitMask)$eq(0)
   mask_sha <- qa$bitwiseAnd(cloudShadowBitMask)$eq(0)
   
-  return(img$updateMask( mask_cir$And(mask_clo)$And(mask_sha) ))
+  return(img$updateMask(mask_cir$And(mask_clo)$And(mask_sha)))
 }
 
 scaleData_LT_SR <- function(img){
   
-  opticalBands = img$select('SR_B.')$multiply(0.0000275)$add(-0.2)
-  return(img$addBands(opticalBands, null, true));
+  opticalBands = img$select(c("Coastal","Blue", "Green", 
+                              "Red", "NIR", "SWIR1", "SWIR2"))$multiply(0.0000275)$add(-0.2)
+  
+  return(img$addBands(opticalBands, NULL, TRUE));
 }
 
 scaleData_LT_TOA <- function(img){
   return(ee$Image(img$multiply(1)))
 }
 
+
 ## ----------------------------------------------------------------------------------- ##
 ## MODIS/Terra/Aqua ----
 ## ----------------------------------------------------------------------------------- ##
 
 
+bitwiseExtract <- function(value, fromBit, toBit = NULL) {
+  
+  if (is.null(toBit)) {
+    toBit <- fromBit
+  }
+  
+  maskSize <- ee$Number(1)$add(toBit)$subtract(fromBit)
+  mask <- ee$Number(1)$leftShift(maskSize)$subtract(1)
+  
+  return(value$rightShift(fromBit)$bitwiseAnd(mask))
+}
 
+maskClouds_MOD09A1 <- function(image) {
+  
+  qa <- image$select("StateQA")
+  
+  cloudState <- bitwiseExtract(qa, 0, 1)
+  cloudShadowState <- bitwiseExtract(qa, 2)
+  cirrusState <- bitwiseExtract(qa, 8, 9)
+  
+  cloud  <- cloudState$eq(0)
+  shadow <- cloudShadowState$eq(0)
+  cirrus <- cirrusState$eq(0)
+  
+  maskedImage <- image$updateMask(cloud$And(shadow)$And(cirrus))
+  
+  return(maskedImage)
+}
 
+maskClouds_MOD13Q1 <- function(image) {
+  
+  qa <- image$select("SummaryQA")
+  
+  goodQualityVI <- bitwiseExtract(qa, 0, 1)
+  mask  <- goodQualityVI$eq(0)
+  
+  maskedImage <- image$updateMask(mask)
+  
+  return(maskedImage)
+}
+
+scaleData_MODIS <- function(img){
+  return(ee$Image(img$multiply(0.0001)))
+}
 
 
