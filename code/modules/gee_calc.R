589c6107b9e935a3d23683f733a16f59c1f7e7dc
João Gonçalves <joaofgo@gmail.com>
2023-03-11 05:19:55 +0000

    Adds a get function to the spectral index, scaling and masking functions

--- code/modules/gee_calc.R
+++ code/modules/gee_calc.R
@@ -1,25 +1,192 @@
 
 
+getSpecIndexFun <- function(baseIndex, satCode=NULL, modisProduct=NULL){
+  
+  if((baseIndex %in% c("TCTB","TCTG","TCTW","NBRP")) && is.null(satCode)){
+    stop("The spectral index is specific to a satellite which has not been defined. Check satCode")
+  }
+  
+  if((baseIndex %in% c("TCTB","TCTG","TCTW")) && satCode %in% c("MOD","MYD")){
+    if(!(modisProduct %in% c("MOD09A1","MYD09A1"))){
+      stop("MODIS product not supported")
+    }
+  }
+  
+  indexFunctions <- list(
+    
+    # General indices
+    NBR     = calc_NBR,
+    NDVI    = calc_NDVI,
+    EVI     = calc_EVI,
+    NBRSWIR = calc_NBRSWIR,
+    MIRBI   = calc_MIRBI,
+    CSI     = calc_CSI,
+    
+    # Satellite specific indices
+    NBRP = list(
+      S2MSI = calc_NBRP_S2
+    ),
+    
+    TCTB = list(
+      S2MSI = calc_TCTB_S2,
+      L5TM  = calc_TCTB_L5,
+      L7ETM = calc_TCTB_L7,
+      L8OLI = calc_TCTB_L8,
+      L9OLI = calc_TCTB_L8,
+      MOD   = calc_TCTB_MOD09A1,
+      MYD   = calc_TCTB_MOD09A1
+    ),
+    
+    TCTG = list(
+      S2MSI = calc_TCTG_S2,
+      L5TM  = calc_TCTG_L5,
+      L7ETM = calc_TCTG_L7,
+      L8OLI = calc_TCTG_L8,
+      L9OLI = calc_TCTG_L8,
+      MOD   = calc_TCTG_MOD09A1,
+      MYD   = calc_TCTG_MOD09A1
+    ),
+    
+    TCTW = list(
+      S2MSI = calc_TCTW_S2,
+      L5TM  = calc_TCTW_L5,
+      L7ETM = calc_TCTW_L7,
+      L8OLI = calc_TCTW_L8,
+      L9OLI = calc_TCTW_L8,
+      MOD   = calc_TCTW_MOD09A1,
+      MYD   = calc_TCTW_MOD09A1
+    )
+  )
+  
+  if(baseIndex %in% c("TCTB","TCTG","TCTW","NBRP")){
+    return(indexFunctions[[baseIndex]][[satCode]])
+  }else{
+    return(indexFunctions[[baseIndex]])
+  }
+}
+
+
+getMaskFun <- function(satCode, modisProduct=NULL){
+  
+  
+  if((satCode %in% c("MOD","MYD")) && is.null(modisProduct)){
+    stop("modisProduct must be defined")
+  }
+  
+  makFuns <- list(
+    
+    S2MSI = maskClouds_S2,
+    
+    L5TM  = maskClouds_LT5,
+    L7ETM = maskClouds_LT7,
+    L8OLI = maskClouds_LT8,
+    L9OLI = maskClouds_LT9,
+    
+    MOD   = list(
+      MOD09A1 = maskClouds_MOD09A1,
+      MOD13Q1 = maskClouds_MOD13Q1
+    ),
+    MYD   = list(
+      MYD09A1 = maskClouds_MOD09A1,
+      MYD13Q1 = maskClouds_MOD13Q1
+    )
+  )
+  
+  if(satCode %in% c("MOD","MYD")){
+    return(indexFunctions[[satCode]][[modisProduct]])
+  }else{
+    return(indexFunctions[[satCode]])
+  }
+}
+
+
+getScaleDataFun <- function(satCode, procLevel=NULL, modisProduct=NULL){
+  
+  
+  if((satCode %in% c("MOD","MYD")) && is.null(modisProduct)){
+    stop("MODIS product name (modisProduct) must be defined")
+  }
+  
+  if((satCode %in% c("L5TM", "L7ETM", "L8OLI", "L9OLI")) && is.null(procLevel)){
+    stop("Processing level (procLevel) must be defined")
+  }
+  
+  makFuns <- list(
+    
+    S2MSI = scaleData_S2,
+    
+    L5TM  = list(
+      L1  = scaleData_LT_TOA,
+      L1C = scaleData_LT_TOA,
+      L2  = scaleData_LT_SR,
+      L2A = scaleData_LT_SR
+    ),
+    
+    L7ETM = list(
+      L1  = scaleData_LT_TOA,
+      L1C = scaleData_LT_TOA,
+      L2  = scaleData_LT_SR,
+      L2A = scaleData_LT_SR
+    ),
+    
+    L8OLI = list(
+      L1  = scaleData_LT_TOA,
+      L1C = scaleData_LT_TOA,
+      L2  = scaleData_LT_SR,
+      L2A = scaleData_LT_SR
+    ),
+    
+    L9OLI = list(
+      L1  = scaleData_LT_TOA,
+      L1C = scaleData_LT_TOA,
+      L2  = scaleData_LT_SR,
+      L2A = scaleData_LT_SR
+    ),
+    
+    MOD   = list(
+      MOD09A1 = scaleData_MODIS,
+      MOD13Q1 = scaleData_MODIS
+    ),
+    
+    MYD   = list(
+      MYD09A1 = scaleData_MODIS,
+      MYD13Q1 = scaleData_MODIS
+    )
+  )
+  
+  if(satCode %in% c("MOD","MYD")){
+    return(indexFunctions[[satCode]][[modisProduct]])
+  }
+  else if(satCode %in% c("L5TM", "L7ETM", "L8OLI", "L9OLI")){
+    return(indexFunctions[[satCode]][[procLevel]])
+  }
+  else{
+    return(indexFunctions[[satCode]])
+  }
+}
+
+
+
 processGEEtask <- function(task, outFolder = "GEE", boundBox, 
                            coordRefSys = 'EPSG:32629'){
   
   # Get run parameters
   mainTaskStatus    = getTaskStatus(task,taskStep = "main")
   
-  satCode           = getSatCode(task)
-  procLevel         = getProcLevel(task)
-  modisProduct      = getModisProduct(task)
-  baseIndex         = getBaseIndex(task)
-  severityIndicator = getSeverityIndicator(task)
+  satCode           = getSatCode(task)                 # Satellite code
+  procLevel         = getProcLevel(task)               # Data processing level L1, L2, ..
+  modisProduct      = getModisProduct(task)            # MODIS product name, eg MOD09A1
+  baseIndex         = getBaseIndex(task)               # Base index name, eg NBR
+  severityIndicator = getSeverityIndicator(task)       # Severity indicator name, eg, DELTA
   
-  burntAreaDataset  = getBurntAreaDataset(task)
-  referenceYear     = getReferenceYear(task)
+  burntAreaDataset  = getBurntAreaDataset(task)        # Burned area dataset used in calculations eg, ICNF, EFFIS
+  referenceYear     = getReferenceYear(task)           # Reference year for calculations
 
-  minFireSizeHa             = getMinFireSize(task)
-  fixedPreFireWindowSize    = getPreFireRef(task)
-  preFireWindowType         = getPreFireType(task)
-  postFireWindowEndMonths   = getPostFireRef(task)
-  postFireWindowDays        = getPostWindowDays(task)
+  minFireSizeHa          = getMinFireSize(task)        # Minimum size of the fire (hectares)
+  fixedPreFireWindowSize = getPreFireRef(task)      
+  preFireWindowType      = getPreFireType(task)
+  postFireWindowEndMonths= getPostFireRef(task)
+  postFireWindowDays     = getPostWindowDays(task)
 
   
   refPeriods = paste0(# Pre-fire ref period
@@ -42,76 +209,128 @@ processGEEtask <- function(task, outFolder = "GEE", boundBox,
     stop("Unsupported burnt area dataset name in burntAreaDataset!")
   }
   
+
+  cloudMaskFun = getCloudMaskFun(satCode, modisProduct)
+    
+  scaleDataFun = getScaleDataFun(satCode, procLevel, modisProduct)
+  
+  baseIndexFun = getSpecIndexFun(baseIndex, satCode, modisProduct)
+  
+  
   # Cloud mask & scale data functions
   #
-  
-  # SENTINEL-2
-  if(satCode == "S2MSI"){
-    cloudMaskFun = maskClouds_S2
-    scaleDataFun = scaleData_S2
-  }
-  
-  # LANDSAT MISSIONS
-  else if(satCode %in% c("L5TM", "L7ETM", "L8OLI", "L9OLI")){
-    
-    if(satCode == "L5TM"){
-      cloudMaskFun = maskClouds_LT5
-    }else if(satCode == "L7ETM"){
-      cloudMaskFun = maskClouds_LT7
-    }else if(satCode == "L8OLI"){
-      cloudMaskFun = maskClouds_LT8
-    }else if(satCode == "L9OLI"){
-      cloudMaskFun = maskClouds_LT9
-    }else{
-      stop("Invalid satCode value in processGEEtask function")
-    }
-    
-    if(procLevel %in% c("L1", "L1C")){
-      scaleDataFun = scaleData_LT_TOA
-    }else if(procLevel %in% c("L2", "L2A")){
-      scaleDataFun = scaleData_LT_SR
-    }else{
-      stop("Invalid procLevel value in processGEEtask function")
-    }
-  }
-  else{
-    stop("Unsupported satellite code in satCode!")
-  }
-  
-  
+  # # SENTINEL-2
+  # if(satCode == "S2MSI"){
+  #   cloudMaskFun = maskClouds_S2
+  #   scaleDataFun = scaleData_S2
+  # }
+  # 
+  # # LANDSAT MISSIONS
+  # else if(satCode %in% c("L5TM", "L7ETM", "L8OLI", "L9OLI")){
+  #   
+  #   if(satCode == "L5TM"){
+  #     cloudMaskFun = maskClouds_LT5
+  #   }else if(satCode == "L7ETM"){
+  #     cloudMaskFun = maskClouds_LT7
+  #   }else if(satCode == "L8OLI"){
+  #     cloudMaskFun = maskClouds_LT8
+  #   }else if(satCode == "L9OLI"){
+  #     cloudMaskFun = maskClouds_LT9
+  #   }else{
+  #     stop("Invalid satCode value in processGEEtask function")
+  #   }
+  #   
+  #   if(procLevel %in% c("L1", "L1C")){
+  #     scaleDataFun = scaleData_LT_TOA
+  #   }else if(procLevel %in% c("L2", "L2A")){
+  #     scaleDataFun = scaleData_LT_SR
+  #   }else{
+  #     stop("Invalid procLevel value in processGEEtask function")
+  #   }
+  # }
+  # else{
+  #   stop("Unsupported satellite code in satCode!")
+  # }
+  #
+  #
   # Spectral indices
-  if(baseIndex == "NBR"){
-    baseIndexFun = calc_NBR
-  }
-  else if(baseIndex == "NDVI"){
-    baseIndexFun = calc_NDVI
-  } 
-  else if(baseIndex == "EVI"){
-    baseIndexFun = calc_EVI
-  } 
+  # if(baseIndex == "NBR"){
+  #   baseIndexFun = calc_NBR
+  # }
+  # else if(baseIndex == "NDVI"){
+  #   baseIndexFun = calc_NDVI
+  # } 
+  # else if(baseIndex == "EVI"){
+  #   baseIndexFun = calc_EVI
+  # } 
+  # else if(baseIndex == "NBRSWIR"){
+  #   baseIndexFun = calc_NBRSWIR
+  # } 
+  # else if(baseIndex == "MIRBI"){
+  #   baseIndexFun = calc_MIRBI
+  # } 
+  # else if(baseIndex == "CSI"){
+  #   baseIndexFun = calc_CSI
+  # } 
+  # else if(baseIndex == "NBRP" & satCode == "S2MSI"){
+  #   baseIndexFun = calc_NBRP_S2
+  # }
+  # 
+  # ## ---- TASSELED CAPS TRANSFORMATIONS ---- ## 
+  # ##
+  # else if(baseIndex == "TCTB"){
+  #   if(satCode == "S2MSI"){
+  #     baseIndexFun = calc_TCTB_S2 
+  #   }
+  #   else if(satCode == "L5TM"){
+  #     baseIndexFun = calc_TCTB_L5
+  #   }
+  #   else if(satCode == "L7ETM"){
+  #     baseIndexFun = calc_TCTB_L7 
+  #   }
+  #   else if(satCode == "L8OLI"){
+  #     baseIndexFun = calc_TCTB_L8 
+  #   }
+  #   else if(satCode == "L9OLI"){
+  #     baseIndexFun = calc_TCTB_L8 
+  #   }
+  #   else{
+  #     stop("Unsupported satCode") 
+  #   }
+  # }
+  # else if(baseIndex == "TCTG"){
+  #   if(satCode == "S2MSI"){
+  #     baseIndexFun = calc_TCTG_S2 
+  #   }
+  #   else if(satCode == "L5TM"){
+  #     baseIndexFun = calc_TCTG_L5
+  #   }
+  #   else if(satCode == "L7ETM"){
+  #     baseIndexFun = calc_TCTG_L7 
+  #   }
+  #   else if(satCode == "L8OLI"){
+  #     baseIndexFun = calc_TCTG_L8 
+  #   }
+  #   else if(satCode == "L9OLI"){
+  #     baseIndexFun = calc_TCTG_L8 
+  #   }
+  #   else{
+  #     stop("Unsupported satCode") 
+  #   }
+  # }
+  # else if(baseIndex == "TCTW"){
+  #   if(satCode == "S2MSI"){
+  #     baseIndexFun = calc_TCTW_S2 
+  #   }
+  # }
+  # ## END TCT
+  # 
+  # else{
+  #   stop("Unsupported spectral index in baseIndex!")
+  # }
+  # 
+  # 
   
-  ## ---- TASSELED CAPS TRANSFORMATIONS ---- ## 
-  ##
-  else if(baseIndex == "TCTB"){
-    if(satCode == "S2MSI"){
-      baseIndexFun = calc_TCTB_S2 
-    }
-  }
-  else if(baseIndex == "TCTG"){
-    if(satCode == "S2MSI"){
-      baseIndexFun = calc_TCTG_S2 
-    }
-  }
-  else if(baseIndex == "TCTW"){
-    if(satCode == "S2MSI"){
-      baseIndexFun = calc_TCTW_S2 
-    }
-  }
-  ## END TCT
-  
-  else{
-    stop("Unsupported spectral index in baseIndex!")
-  }
   
   # Get the spatial resolution of the data
   spatialRes <- getSpatialResolution(task)
