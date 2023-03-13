94614e4af2015dc392cb2926658e1bfd3e78cf31
João Gonçalves <joaofgo@gmail.com>
2023-03-13 15:31:59 +0000

    Correct bug on EFFIS upload via gcs

--- code/update_effis_asset.R
+++ code/update_effis_asset.R
@@ -3,18 +3,9 @@ library(sf)
 library(rgee)
 library(dplyr)
 
-ee_Initialize(user = "joaofgo", gcs = TRUE)
+source("./code/modules/r_utils.R")
 
-
-
-download_and_unzip <- function(url, dest_file, extdir) {
-  
-  download.file(url, dest_file, mode = "wb")
-  
-  if (tools::file_ext(dest_file) == "zip") {
-    unzip(dest_file, exdir = extdir, overwrite = TRUE)
-  }
-}
+ee_Initialize(user = "joaofgo@gmail.com", gcs = TRUE)
 
 
 url <- "https://maps.wild-fire.eu/effis?service=WFS&request=getfeature&typename=ms:modis.ba.poly&version=1.1.0&outputformat=SHAPEZIP"
@@ -22,22 +13,32 @@ url <- "https://maps.wild-fire.eu/effis?service=WFS&request=getfeature&typename=
 download_and_unzip(url, "./temp/effis_data.zip", "./temp")
 
 
-
 aa <- read_sf("./temp/modis.ba.poly.shp")
 
 aapt <- aa %>% 
   filter(COUNTRY == "PT") %>% 
-  mutate(year = substr(FIREDATE,1,4))
+  mutate(year = substr(FIREDATE,1,4)) %>% 
+  mutate(AREA_HA = as.numeric(AREA_HA)) %>% 
+  st_make_valid() %>% 
+  st_buffer(0.0) %>% 
+  mutate(area_ht = as.numeric(st_area(.)/10000))
 
 
-# aapt %>% select(year) %>% st_drop_geometry()
+dir.create(paste("./data/vector/EFFIS",getDateString(),sep="_"))
+
+shpFile <- paste("./data/vector/EFFIS_",getDateString(),
+                 "/EFFIS_",getDateString(),".shp",sep="")
+
+write_sf(aapt, shpFile)
+aapt_ <- read_sf(shpFile)
+
 
 sf_as_ee(
-  aapt,
+  aapt_,
   via = "gcs_to_asset",
   assetId = "users/joaofgo/severus_pt/effis_test",
   bucket = "my_gee_data_bucket",
-  predefinedAcl = "joaofgo",
+  predefinedAcl = "bucketLevel",
   command_line_tool_path = NULL,
   overwrite = TRUE,
   monitoring = TRUE,
