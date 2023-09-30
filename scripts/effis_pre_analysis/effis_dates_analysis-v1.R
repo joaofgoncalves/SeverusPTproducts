47e0a5d2816e22a7ccf0e7376887966ca586ca94
João Gonçalves <joaofgo@gmail.com>
2023-09-30 17:30:46 +0100

    Improved plotting

--- scripts/effis_pre_analysis/effis_dates_analysis-v1.R
+++ scripts/effis_pre_analysis/effis_dates_analysis-v1.R
@@ -37,10 +37,10 @@ wrong_date_counts_all <-
 wrong_date_counts <- aapt %>%
   st_drop_geometry() %>%
   select(year, area_ht, fire_dt) %>%
-  mutate(is_wrong_date = ifelse(as.character(fire_dt) ==
+  mutate(date = ifelse(as.character(fire_dt) ==
                                   paste0(year,"-01-01"),
-                                "WRONG DATE","GOOD DATE")) %>%
-  group_by(year, is_wrong_date, .drop = FALSE) %>%
+                                "WRONG","GOOD")) %>%
+  group_by(year, date, .drop = FALSE) %>%
   summarise(nr_fires = n(), area_group = sum(area_ht)) %>%
   left_join(wrong_date_counts_all, by="year") %>%
   as.data.frame() %>%
@@ -54,29 +54,47 @@ wrong_date_counts_long <- wrong_date_counts %>%
 
 
 g1 <- ggplot(wrong_date_counts_long %>% filter(name == "p_area"),
-            aes(x=year, y=value, fill=is_wrong_date)) +
+            aes(x=year, y=value, fill=date)) +
   geom_bar(stat = "identity") +
   theme_bw() +
-  theme(axis.text.x = element_text(angle=90, vjust = 0.5))
+  theme(axis.text.x = element_text(angle=90, vjust = 0.5)) +
+  theme(legend.position = "bottom") +
+  xlab("Year") +
+  ylab("% of annual burned area")
 
 plot(g1)
 
 g2 <- ggplot(wrong_date_counts_long %>% filter(name == "p_nr_fires"),
-             aes(x=year, y=value, fill=is_wrong_date)) +
+             aes(x=year, y=value, fill=date)) +
   geom_bar(stat = "identity") +
   theme_bw() +
-  theme(axis.text.x = element_text(angle=90, vjust = 0.5))
+  theme(axis.text.x = element_text(angle=90, vjust = 0.5)) +
+  theme(legend.position = "bottom") +
+  xlab("Year") +
+  ylab("% of annual wildfires")
 
 plot(g2)
 
 g3 <- ggplot(wrong_date_counts_long %>% filter(name == "nr_fires"),
-             aes(x=year, y=value, fill=is_wrong_date)) +
+             aes(x=year, y=value, fill=date)) +
   geom_bar(stat = "identity") +
   theme_bw() +
-  theme(axis.text.x = element_text(angle=90, vjust = 0.5))
+  theme(axis.text.x = element_text(angle=90, vjust = 0.5)) +
+  theme(legend.position = "bottom") +
+  xlab("Year") +
+  ylab("% of annual burned area") +
+  xlab("Year") +
+  ylab("Number of annual wildfires")
 
 plot(g3)
 
+library(patchwork)
+
+m <- g1 | g2 | g3
+
+plot(m)
+
+ggsave("./out/EFFIS_wrong_dates.png", m, width = 13, height = 6)
 
 ## ---------------------------------------------------------------------------------------- ##
 
