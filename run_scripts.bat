ff024fabaf14f20f8b53addf7c027b1edefb8fd7
João Gonçalves <joaofgo@gmail.com>
2023-09-30 17:20:00 +0100

    Updates the batch file for R 4.3

--- run_scripts.bat
+++ run_scripts.bat
@@ -2,4 +2,4 @@
 @echo off
 cd C:/MyFiles/R-dev/SeverusPTproducts
 
-"C:/Program Files/R/R-4.2.2/bin/Rscript.exe" ./scripts/run_products_tasks_parallel.R --verbose
+"C:/Program Files/R/R-4.3.1/bin/Rscript.exe" ./scripts/run_products_tasks_parallel-v2.R --verbose
